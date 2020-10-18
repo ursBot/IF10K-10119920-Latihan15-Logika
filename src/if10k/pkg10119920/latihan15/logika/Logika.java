4ba4e40c876de06ab9a50b4dd71c03cc5816fe5b
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:23:04 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan15/logika/Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan15.logika;
+
+
+/**
+*
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Logika{
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+
+}
